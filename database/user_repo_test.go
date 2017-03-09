b2c429db2d9c252d256267582ad2a13ab7040cdc
Joseph Spurrier <github@josephspurrier.com>
2017-03-08 23:14:52 -0500

    Test storage failures.

--- database/user_repo_test.go
+++ database/user_repo_test.go
@@ -24,3 +24,27 @@ func TestUserRepo(t *testing.T) {
 	_, err = s.FindByEmail("jdoe@example.com")
 	AssertEqual(t, err, nil)
 }
+
+// TestUserRepoFail tests the user repo.
+func TestUserRepoFail(t *testing.T) {
+	db := new(database.MockService)
+	s := database.NewUserRepo(db)
+
+	db.WriteFail = true
+	u := new(domain.User)
+	u.Email = "jdoe@example.com"
+	u.Password = "Pa$$w0rd"
+	err := s.Store(u)
+	AssertNotNil(t, err)
+
+	db.WriteFail = false
+	err = s.Store(u)
+	AssertEqual(t, err, nil)
+
+	_, err = s.FindByEmail("jdoe@example.com")
+	AssertEqual(t, err, nil)
+
+	db.ReadFail = true
+	_, err = s.FindByEmail("jdoe@example.com")
+	AssertNotNil(t, err)
+}
