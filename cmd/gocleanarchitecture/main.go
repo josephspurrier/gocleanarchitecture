849e76aa5489da6f28c89e1a5798cd0973432664
Joseph Spurrier <github@josephspurrier.com>
2017-03-15 01:11:16 -0400

    Simplify the main function.

--- cmd/gocleanarchitecture/main.go
+++ cmd/gocleanarchitecture/main.go
@@ -9,17 +9,12 @@ import (
 
 // main is the entrypoint for the application.
 func main() {
-	// Register all services.
-	s := boot.RegisterServices()
-
-	// Load all the services into the controller handlers and return the
-	// handler with all the routes.
-	h := s.LoadRoutes()
+	// Register the services and load the routes.
+	http.Handle("/", boot.RegisterServices().LoadRoutes())
 
 	// Display message on the server.
 	log.Println("Server started.")
 
 	// Run the web listener.
-	http.Handle("/", h)
 	http.ListenAndServe(":8080", nil)
 }
